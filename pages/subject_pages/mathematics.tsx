68f0336824326f0ca51d55866ec131d454fe481e
Daggerpov <danielagapov1@gmail.com>
2022-11-26 02:26:30 -0800

    fixing import

--- pages/subject_pages/mathematics.tsx
+++ pages/subject_pages/mathematics.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import styles from "../../styles/Home.module.css";
 
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
