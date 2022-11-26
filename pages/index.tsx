5cf2c5355d2b0f09cdc48536c7af7b9ebb1c5fed
Daggerpov <danielagapov1@gmail.com>
2022-11-26 13:45:50 -0800

    starting subjectPages, header, and right sidebar

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,10 @@ import * as React from "react";
 import Image from "next/image";
 import Head from "next/head";
 
+import Container from "@mui/material/Container";
+
+import AdbIcon from "@mui/icons-material/Adb";
+import ToolTip from "@mui/material/ToolTip";
 import IconButton from "@mui/material/IconButton";
 import Box from "@mui/material/Box";
 import { useTheme, ThemeProvider, createTheme } from "@mui/material/styles";
@@ -13,10 +17,52 @@ import Brightness7Icon from "@mui/icons-material/Brightness7";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import NightlightIcon from "@mui/icons-material/Nightlight";
 import { Button } from "@material-tailwind/react";
+import Drawer from "@mui/material/Drawer";
+import AppBar from "@mui/material/AppBar";
+import CssBaseline from "@mui/material/CssBaseline";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import ScienceTwoToneIcon from '@mui/icons-material/ScienceTwoTone';
+import SportsHandballTwoToneIcon from '@mui/icons-material/SportsHandballTwoTone';
+import CalculateTwoToneIcon from '@mui/icons-material/CalculateTwoTone';
+import HomeTwoToneIcon from '@mui/icons-material/HomeTwoTone';
+import DescriptionTwoToneIcon from '@mui/icons-material/DescriptionTwoTone';
+
+import IconButton from "@mui/material/IconButton";
+import Box from "@mui/material/Box";
+import { useTheme, ThemeProvider, createTheme } from "@mui/material/styles";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import NightlightIcon from "@mui/icons-material/Nightlight";
+import { Button } from "@material-tailwind/react";
+import Drawer from "@mui/material/Drawer";
+import AppBar from "@mui/material/AppBar";
+import CssBaseline from "@mui/material/CssBaseline";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import ScienceTwoToneIcon from '@mui/icons-material/ScienceTwoTone';
+import SportsHandballTwoToneIcon from '@mui/icons-material/SportsHandballTwoTone';
+import CalculateTwoToneIcon from '@mui/icons-material/CalculateTwoTone';
+import HomeTwoToneIcon from '@mui/icons-material/HomeTwoTone';
+import DescriptionTwoToneIcon from '@mui/icons-material/DescriptionTwoTone';
 
 import styles from "../styles/Home.module.css";
 
+import Link from "next/link";
+
 const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+const drawerWidth = 220;
 
 function Dashboard() {
     const theme = useTheme();
@@ -44,8 +90,229 @@ function Dashboard() {
                     />
                 </Head>
                 <main className={styles.main}>
+                    <AppBar position="static">
+                        <Container maxWidth="xl">
+                            <Toolbar>
+                                <AdbIcon
+                                    sx={{
+                                        display: { xs: "none", md: "flex" },
+                                        mr: 1,
+                                    }}
+                                />
+                                <Typography
+                                    variant="h6"
+                                    noWrap
+                                    component="a"
+                                    href="/"
+                                    sx={{
+                                        mr: 2,
+                                        display: { xs: "none", md: "flex" },
+                                        fontFamily: "monospace",
+                                        fontWeight: 700,
+                                        letterSpacing: ".3rem",
+                                        color: "inherit",
+                                        textDecoration: "none",
+                                    }}
+                                >
+                                    TRANSLANG - THE ONLINE CODE CONVERTER
+                                </Typography>
+
+                                <Box
+                                    sx={{
+                                        flexGrow: 1,
+                                        display: { xs: "flex", md: "none" },
+                                    }}
+                                ></Box>
+                                <Box
+                                    sx={{
+                                        flexGrow: 1,
+                                        display: { xs: "none", md: "flex" },
+                                    }}
+                                ></Box>
+
+                                <Box sx={{ flexGrow: 0, px: "15px" }}>
+                                    <Tooltip title="Open settings">
+                                        <Button
+                                            variant="contained"
+                                            onClick={handleOpenUserMenu}
+                                        >
+                                            <Typography
+                                                textAlign="center"
+                                                sx={{ mr: "5px" }}
+                                            >
+                                                {auth.currentUser.displayName}
+                                            </Typography>
+                                            <Avatar
+                                                alt="Google Photo/Initial"
+                                                src={auth.currentUser.photoURL}
+                                            />
+                                        </Button>
+                                    </Tooltip>
+                                    <Menu
+                                        sx={{ mt: "0" }}
+                                        id="menu-appbar"
+                                        anchorEl={anchorElUser}
+                                        anchorOrigin={{
+                                            vertical: "bottom",
+                                            horizontal: "right",
+                                        }}
+                                        keepMounted
+                                        transformOrigin={{
+                                            vertical: "top",
+                                            horizontal: "right",
+                                        }}
+                                        open={Boolean(anchorElUser)}
+                                        onClose={handleCloseUserMenu}
+                                    >
+                                        <Link
+                                            href={{
+                                                pathname: "/menu/inbox",
+                                            }}
+                                        >
+                                            <MenuItem key="Inbox">
+                                                <InboxIcon
+                                                    sx={{ pr: "5px" }}
+                                                ></InboxIcon>
+                                                <Typography textAlign="center">
+                                                    Inbox
+                                                </Typography>
+                                            </MenuItem>
+                                        </Link>
+                                        <Link
+                                            href={{
+                                                pathname:
+                                                    "/menu/submitted-complaints",
+                                            }}
+                                        >
+                                            <MenuItem
+                                                key="Submitted Complaints"
+                                                onClick={handleCloseUserMenu}
+                                            >
+                                                <UploadFileIcon
+                                                    sx={{ pr: "5px" }}
+                                                ></UploadFileIcon>
+                                                <Typography textAlign="center">
+                                                    Submitted Complaints
+                                                </Typography>
+                                            </MenuItem>
+                                        </Link>
+                                        <Link
+                                            href={{
+                                                pathname:
+                                                    "/menu/accepted-suggestions",
+                                            }}
+                                        >
+                                            <MenuItem
+                                                key="Accepted Suggestions"
+                                                onClick={handleCloseUserMenu}
+                                            >
+                                                <DoneAllIcon
+                                                    sx={{ pr: "5px" }}
+                                                ></DoneAllIcon>
+                                                <Typography textAlign="center">
+                                                    Accepted Suggestions
+                                                </Typography>
+                                            </MenuItem>
+                                        </Link>
+                                    </Menu>
+                                </Box>
+                                {user && (
+                                    // <Link
+                                    //     href={{
+                                    //         pathname: "/auth/login",
+                                    //     }}
+                                    // >
+                                    <Button
+                                        sx={{ px: "15px" }}
+                                        // style={}
+                                        onClick={() => {
+                                            logout();
+                                        }}
+                                        color="inherit"
+                                        variant="outlined"
+                                        className="btn"
+                                    >
+                                        Sign Out
+                                    </Button>
+                                    // </Link>
+                                )}
+                            </Toolbar>
+                        </Container>
+                    </AppBar>
                     <h1 className={styles.title}>Cram Tracker</h1>
-                    
+                    {/* here put drawer */}
+                    <Box sx={{ display: "flex" }}>
+                        <CssBaseline />{" "}
+                        {/* check if this line can be removed */}
+                        <Drawer
+                            variant="permanent"
+                            sx={{
+                                width: drawerWidth,
+                                flexShrink: 0,
+                                [`& .MuiDrawer-paper`]: {
+                                    width: drawerWidth,
+                                    boxSizing: "border-box",
+                                },
+                            }}
+                        >
+                            <Toolbar />
+                            <Box sx={{ overflow: "auto", marginTop: 10 }}>
+                                <List>
+                                    <ListItem key={"Home"} disablePadding>
+                                        <ListItemButton>
+                                            <ListItemIcon>
+                                                <HomeTwoToneIcon />
+                                            </ListItemIcon>
+                                            <ListItemText primary={"Home"} />
+                                        </ListItemButton>
+                                    </ListItem>
+                                </List>
+                                <List>
+                                    <ListItem key={"English"} disablePadding>
+                                        <ListItemButton>
+                                            <ListItemIcon>
+                                                <DescriptionTwoToneIcon />
+                                            </ListItemIcon>
+                                            <ListItemText primary={"English"} />
+                                        </ListItemButton>
+                                    </ListItem>
+                                </List>
+                                <List>
+                                    <ListItem key={"Math"} disablePadding>
+                                        <ListItemButton>
+                                            <ListItemIcon>
+                                                <CalculateTwoToneIcon />
+                                            </ListItemIcon>
+                                            <ListItemText primary={"Math"} />
+                                        </ListItemButton>
+                                    </ListItem>
+                                </List>
+                                <List>
+                                    <ListItem key={"Physics"} disablePadding>
+                                        <ListItemButton>
+                                            <ListItemIcon>
+                                                <SportsHandballTwoToneIcon />
+                                            </ListItemIcon>
+                                            <ListItemText primary={"Physics"} />
+                                        </ListItemButton>
+                                    </ListItem>
+                                </List>
+                                <List>
+                                    <ListItem key={"Chemistry"} disablePadding>
+                                        <ListItemButton>
+                                            <ListItemIcon>
+                                                <ScienceTwoToneIcon />
+                                            </ListItemIcon>
+                                            <ListItemText
+                                                primary={"Chemistry"}
+                                            />
+                                        </ListItemButton>
+                                    </ListItem>
+                                </List>
+                            </Box>
+                        </Drawer>
+                    </Box>
+                    {/* //toggle theme button */}
                     {theme.palette.mode} mode
                     <IconButton
                         sx={{ ml: 1 }}
--- pages/testDrawer.tsx
+++ pages/testDrawer.tsx
@@ -1,11 +1,8 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
-import AppBar from '@mui/material/AppBar';
-import CssBaseline from '@mui/material/CssBaseline';
-import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
 import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
@@ -14,39 +11,76 @@ import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 
-const drawerWidth = 240;
+type Anchor = 'right';
 
-export default function ClippedDrawer() {
-  return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
+export default function TemporaryDrawer() {
+  const [state, setState] = React.useState({
+    right: false,
+  });
 
-      <Drawer
-        variant="permanent"
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
-        }}
-      >
-        <Toolbar />
-        <Box sx={{ overflow: 'auto' , marginTop: 10}}>
-          <List>
-            {['Home', 'Writing', 'Mathematics', 'Physics', 'Chemistry'].map((text, index) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                  {index % 2 == 0 ? <InboxIcon /> : <MailIcon />}
-                  </ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItemButton>
-              </ListItem>
-              
-            ))}
-          </List>
-         
-        </Box>
-      </Drawer>
+  const toggleDrawer =
+    (anchor: Anchor, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return;
+      }
+
+      setState({ ...state, [anchor]: open });
+    };
+
+  const list = (anchor: Anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
     </Box>
+  );
+
+  return (
+    <div>
+      {(['right'] as const).map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </div>
   );
 }
